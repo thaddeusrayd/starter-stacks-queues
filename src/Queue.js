f83732c38a43e2bf75f682ed9b4da340f2991152
thaddeusrayd <thaddeusrayd@gmail.com>
2021-11-10 09:35:12 -0600

    dequeue added

---
+++ src/Queue.js
@@ -0,0 +1,44 @@
+// creates a node containing the data and a reference to the next item
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.first) {
+      this.last.next = newNode;
+    } else {
+      // set the node of the queue's next pointer to be the new node
+      this.first = newNode;
+    }
+
+    // make the new node the last item on the queue
+    this.last = newNode;
+  }
+
+  dequeue() {
+    if (this.first) {
+      const dequeued = this.first;
+
+      // update first pointer to point to the next node of the dequeued node
+      this.first = dequeued.next;
+
+      // if dequeued node is the last node in the queue,
+      // update the last pointer to point to null
+      if (dequeued === this.last) {
+        this.last = null;
+      }
+      return dequeued.value;
+    }
+  }
+}
